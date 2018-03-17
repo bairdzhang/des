47425029d32dad457c6ed1357cfa91e93b277894
Zhishuai Zhang <zhshuai.zhang@gmail.com>
2018-03-17 15:16:40 +0000

    update

--- jobs/VGGNet/VOC0712/DES512x512withCOCO/DES512x512withCOCO_test.sh
+++ jobs/VGGNet/VOC0712/DES512x512withCOCO/DES512x512withCOCO_test.sh
@@ -1,5 +1,5 @@
 cd /workspace/release/caffe-ssd
 ./build/tools/caffe train \
 --solver="models/VGGNet/VOC0712/DES512x512withCOCO/solver_test.prototxt" \
---weights="models/VGGNet/VOC0712/DES512x512withCOCO/VGG_VOC0712_SSD_512x512_ft_iter_160000.caffemodel" \
+--weights="models/VGGNet/VOC0712/DES512x512withCOCO/VGG_VOC0712_SSD_512x512_ft_iter_120000.caffemodel" \
 --gpu 0,1 2>&1 | tee jobs/VGGNet/VOC0712/DES512x512withCOCO/DES512x512withCOCO_test.log
